6db2774b984144a0c38efcb1c27792add354c161
seryio2004 <86850969+seryio2004@users.noreply.github.com>
2025-02-12 18:13:04 +0100

    calcularHercios

---
+++ scripts.js
@@ -0,0 +1,57 @@
+
+let audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+let oscillator, gainNode, freq;
+
+
+document.querySelector("#freq-slider").addEventListener("input", (e) => {
+    freq = e.target.value;
+    document.querySelector("#freq-label").textContent = freq;
+
+});
+
+
+function playNote(frequency) {
+    stopNote();
+    oscillator = audioCtx.createOscillator();
+    gainNode = audioCtx.createGain();
+    
+    oscillator.type = 'sawtooth'; // Tipo de onda
+    oscillator.frequency.value = frequency;
+    gainNode.gain.value = 0.3;
+
+    oscillator.connect(gainNode);
+    gainNode.connect(audioCtx.destination);
+
+    oscillator.start();
+}
+function calcularHercios(nota){
+    switch(nota){
+        case "do":
+            return freq;
+        case "re":
+            return freq * (2 **(2/12));
+        case "mi":
+            return freq * (2 **(4/12));
+        case "fa":
+            return freq * (2 **(5/12));
+        case "sol":
+            return freq * (2 **(7/12));
+        case "la":
+            return freq * (2 **(9/12));
+        case "si":
+            return freq * (2 **(11/12));
+        case "do2":
+            return freq * (2 **(12/12));
+    }
+}
+
+function stopNote() {
+    if (oscillator) {
+        oscillator.stop();
+        oscillator.disconnect();
+    }
+}
+
+
+
+document.body.addEventListener("mouseup", stopNote);
